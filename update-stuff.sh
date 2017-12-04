8c33da019d00347e66be71cecddeee94e8d5d52b
Conrad Wood <cnw@singingcat.net>
2017-12-04 23:05:51 +0000

    .

--- update-stuff.sh
+++ update-stuff.sh
@@ -4,3 +4,9 @@ mkdir -p ${TDIR}
 rsync -pvra --delete /home/cnw/devel/picoservices/src/golang.conradwood.net/ ${TDIR}
 ( cd $TDIR ; find -name '*.go' |xargs -n1 git add )
 
+#!/bin/sh
+
+TDIR=src/golang.conradwood.net/vendor/golang.conradwood.net/
+mkdir -p ${TDIR}
+rsync -pvra /home/cnw/devel/logservice/src/golang.conradwood.net/ ${TDIR}
+( cd $TDIR ; find -name '*.go' |xargs -n1 git add )
