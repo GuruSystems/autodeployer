175c49508de380ba5abcb9296e0b6d26c958ada8
Conrad Wood <cnw@singingcat.net>
2018-01-03 11:11:08 +0000

    .

--- autobuild.sh
+++ autobuild.sh
@@ -10,14 +10,15 @@ export GOBIN=`pwd`/dist
 export GOPATH=`pwd`
 echo "GOPATH=$GOPATH"
 mkdir $GOBIN
+MYSRC=src/golang.conradwood.net/deploymonkey
+( cd ${MYSRC} && make proto ) || exit 10
+
 MYSRC=src/golang.conradwood.net/autodeployer
 ( cd ${MYSRC} && make proto ) || exit 10
 ( cd ${MYSRC} && make client ) || exit 10
 ( cd ${MYSRC} && make server ) || exit 10
 cp -rvf ${MYSRC}/proto dist/
 
-MYSRC=src/golang.conradwood.net/deploymonkey
-( cd ${MYSRC} && make proto ) || exit 10
 ( cd ${MYSRC} && make client ) || exit 10
 ( cd ${MYSRC} && make server ) || exit 10
 cp -rvf ${MYSRC}/proto dist/
