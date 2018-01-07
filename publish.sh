bb2f212c45092da65d3a16ad9a437d0a0a1acb4f
Conrad Wood <cnw@singingcat.net>
2018-01-07 03:25:07 +0000

    .

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 # a hack because we don't upgrade all servers automatically yet
 
 rsync -pvra --progress /tmp/go/autodeployer-server lb1:/tmp/
-ssh lb1 "sudo systemctl stop autodeployer ; sudo cp /tmp/autodeployer-server /usr/local/bin/ ; sudo systemctl start autodeployer"
+ssh lb1 "sudo systemctl stop autodeployer ; sleep 1 ;  sudo cp /tmp/autodeployer-server /usr/local/bin/ ; sudo systemctl start autodeployer"
